28d97b2b1cb94bc219a60f3f37ab6a9bf92bc146
Balasundararao.Thunga <bthunga@cisco.com>
2017-11-01 19:34:21 -0700

    updated files

--- sanbaltar/src/app/admin-module/admin-module.module.ts
+++ sanbaltar/src/app/admin-module/admin-module.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatSidenavModule, MatButtonModule, MatIconModule, MatTableModule} from '@angular/material';
 
 import { AdminModuleRoutingModule } from './admin-module-routing.module';
 import { AdminModuleComponent } from './admin-module.component';
-import { MatSidenavModule, MatButtonModule, MatIconModule} from '@angular/material';
 import { AdmCoursesComponent } from './adm-courses/adm-courses.component';
 import { AdmInstructorsComponent } from './adm-instructors/adm-instructors.component';
 import { AdmStudentsComponent } from './adm-students/adm-students.component';
@@ -14,7 +14,8 @@ import { AdmStudentsComponent } from './adm-students/adm-students.component';
     AdminModuleRoutingModule,
     MatSidenavModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatTableModule
   ],
   declarations: [AdminModuleComponent, AdmCoursesComponent, AdmInstructorsComponent, AdmStudentsComponent]
 })
